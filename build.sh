4a0518c032e66cf350e4171cd8628dffd81e1dd7
John Estabrook <jestabro@vyos.io>
2023-07-25 15:02:15 -0500

    libvyosconfig: T5400: pin OCaml package before build

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-DIR=$1
-
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#c5863024 -y'
 eval `opam config env`
 make clean
 make

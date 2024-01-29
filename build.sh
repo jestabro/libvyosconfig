2ef46f22c09252cc5ea36acbc5b4455501bc19a2
John Estabrook <jestabro@vyos.io>
2024-01-28 21:29:21 -0600

    T5528: build requisites from working repos and vyos1x-adapter package

--- build.sh
+++ build.sh
@@ -2,6 +2,18 @@
 
 DIR=$1
 
+git clone https://github.com/jestabro/vyatta-cfg.git /tmp/vyatta-cfg
+cd /tmp/vyatta-cfg && git checkout b9c36eaa
+
+mk-build-deps -i -r -s sudo -t 'apt-get -y -o Debug::pkgProblemResolver=yes --no-install-recommends'
+dpkg-buildpackage -uc -us -tc -b
+sudo sh -c 'dpkg --force-all -i /tmp/libvyatta-cfg1_*.deb /tmp/libvyatta-cfg-dev_*.deb'
+
+cd ${OLDPWD}
+
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/jestabro/vyos1x-config.git#98230f48 -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-adapter https://github.com/jestabro/vyos1x-adapter.git -y'
+
 eval `opam config env`
 make clean
 make

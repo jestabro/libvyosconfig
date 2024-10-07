62aae53f4e313450ffd4c74de83e86b7faee525d
John Estabrook <jestabro@vyos.io>
2024-10-06 19:11:35 -0500

    T6718: vyconf: add temp build.sh with ref to development branches

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 DIR=$1
 
+# Build for testing uses specific repos/commit ids:
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/jestabro/vyos1x-config.git#b2297f819 -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/jestabro/vyconf.git#010aed73c6 -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-adapter https://github.com/jestabro/vyos1x-adapter.git#caf33373bd -y'
+
 eval `opam config env`
 make clean
 make

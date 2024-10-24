41609019df53e762b5e1a55c7f10b246a7763145
John Estabrook <jestabro@vyos.io>
2024-10-24 12:06:41 -0500

    T6718: pin required packages in build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 DIR=$1
 
+# XXX: adjust commit hashes once working repos merged
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/jestabro/vyos1x-config.git#ee9a076026bdf -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/jestabro/vyconf.git#03910a76c978 -y'
+
 eval `opam config env`
 make clean
 make

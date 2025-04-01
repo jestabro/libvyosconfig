ad52c9b00af2316f570db7a46cc3fb7a390652fc
John Estabrook <jestabro@vyos.io>
2025-04-01 12:26:46 -0500

    T7272: update hash for post commit update of config tree

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 DIR=$1
 
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#6f272cd81318e719d91dcc06b1a5cfb435e1f4c7 -y'
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#920b75a6dbf9eac4b94600b759818b2ec28004ae -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#8c1474f99481f5226b1d47d1690b34d9740bf23b -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#aedecf6fa96509ef59fd2e1f607a8a13c18d214b -y'
 
 eval `opam config env`
 make clean

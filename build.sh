6105c3940851069d5645f5558e39fb349fa68a6b
John Estabrook <jestabro@vyos.io>
2025-04-01 18:33:30 -0500

    temp

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 DIR=$1
 
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#8c1474f99481f5226b1d47d1690b34d9740bf23b -y'
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#aedecf6fa96509ef59fd2e1f607a8a13c18d214b -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#1f82c64395bd4a29fe93e0affbefb572259ee736 -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#cb4ccf15b2b24e61ca7ddc3bcdf7ab3acd7bd98e -y'
 
 eval `opam config env`
 make clean

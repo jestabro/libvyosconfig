001011e3ce4e921842c98371cceac7ed9e0ef5b0
John Estabrook <jestabro@vyos.io>
2025-04-03 10:22:20 -0500

    T7292: update hash for set tag/leaf data on configured paths

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 DIR=$1
 
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#8c1474f99481f5226b1d47d1690b34d9740bf23b -y'
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#aedecf6fa96509ef59fd2e1f607a8a13c18d214b -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#ccdc583f0f02583771fadae353166ef1a904f4e3 -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#0fb5e2f781725196ae010cf64e4d185a8f5ce721 -y'
 
 eval `opam config env`
 make clean

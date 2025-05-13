1475cdb3fe245e2ba5de08ee48891e7cbb3c3729
John Estabrook <jestabro@vyos.io>
2025-05-13 08:29:06 -0500

    T7363: update hash for vyconf aware initialization of Config

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 DIR=$1
 
 sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#e1b570dfbdb5703688b445f9d54160754cc2d190 -y'
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#9c0377200774cf8351dde9e134634aef85381674 -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#014a390488aa8d6601f907f8a691878f3583b5e8 -y'
 
 eval `opam config env`
 make clean

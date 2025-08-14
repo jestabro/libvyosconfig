fa48b7126f3a893e519082df5bf9d600bc9753dc
John Estabrook <jestabro@vyos.io>
2025-08-14 13:18:43 -0500

    update for lexical insertion

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 DIR=$1
 
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#868b906e8af275a7121b59bb186c06602e9d8af4 -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#6af7ee2708a69ccb5a301d4a701beaf62d559572 -y'
 sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyconf https://github.com/vyos/vyconf.git#29e73893a0fc942a96abc06dfa037afd71778d27 -y'
 
 eval `opam config env`
